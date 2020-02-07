1f968e889030ef8b34d28b21907c57a46a02a9d0
Ezequiel Portela <ezequiel.portela@naranja.com>
2020-02-07 19:05:25 -0300

    Add jquery

--- client/main.jsx
+++ client/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
 import App from '/imports/ui/App';
+import 'jquery/dist/jquery';
 import 'materialize-css/dist/css/materialize.min.css';
 import 'materialize-css/dist/js/materialize.min.js';
 import '../imports/startup/accounts-config.js';
