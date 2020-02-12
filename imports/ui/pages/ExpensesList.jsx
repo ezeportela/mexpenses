9730ee38155b9b95e383918044db265ac1c83e8a
Ezequiel Portela <ezequiel.portela@naranja.com>
2020-02-12 17:06:46 -0300

    checkbox

--- imports/ui/pages/ExpensesList.jsx
+++ imports/ui/pages/ExpensesList.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Meteor } from 'meteor/meteor';
 import { withTracker } from 'meteor/react-meteor-data';
 import { Expenses } from '../../api/expenses';
@@ -6,17 +6,36 @@ import Container from '../components/Container';
 import Card from '../components/Card';
 
 const ExpensesList = props => {
-  const makeListItem = expenses => {
-    const to = `/expenses/${expenses._id}/edit`;
+  const [checkedList, setCheckedList] = useState([]);
+
+  handleCheckboxChange = e => {
+    setCheckedList({
+      ...checkedList,
+      [e.target.value]: e.target.checked
+    });
+  };
+
+  const makeListItem = expense => {
     return (
-      <Card key={expenses._id} title={expenses.accountName}>
-        <p>{expenses.price}</p>
-        <p>{expenses.period}</p>
+      <Card key={expense._id} title={expense.accountName}>
+        <form>
+          <label>
+            <input
+              type="checkbox"
+              className="filled-in"
+              value={expense._id}
+              onChange={handleCheckboxChange}
+            />
+            <span></span>
+          </label>
+        </form>
+        <p>{expense.price}</p>
+        <p>{expense.period}</p>
       </Card>
     );
   };
 
-  const expenses = props.expenses.map(expenses => makeListItem(expenses));
+  const expenses = props.expenses.map(expense => makeListItem(expense));
 
   return (
     <Container>
